648be239de2c63ba416df8cae98618b24550251e
fanqilong <fanqilong_moli@163.com>
2018-09-05 18:17:09 +0800

    Initial commit

---
+++ .umirc.js
@@ -0,0 +1,34 @@
+export default {
+  history: 'hash',
+  hash: true,
+
+  publicPath: "/static/",
+  proxy: {
+    "/api": {
+      "target": "http://jsonplaceholder.typicode.com/",
+      "changeOrigin": true,
+      "pathRewrite": {"^/api": ""}
+    }
+  },
+  theme: {
+    "primary-color": "#f9b016",
+    // '@icon-url': '"/iconfont/iconfont"'
+  },
+  plugins:[
+    ['umi-plugin-react', {
+      dva: true,
+      antd: true,  // antd 默认不开启，如有使用需自行配置
+      routes: {   //基于 umi-plugin-routes 实现，用于批量修改路由。
+        exclude: [/models\//],
+      },
+      polyfills: ['ie9'],//目前只能配置 ['ie9']，实现一键兼容到 IE9。
+      locale: {},
+      library: 'react',
+      hardSource: false,
+      // pwa: {},
+      // hd: true,
+      fastClick: true
+    }],
+  ]
+
+}
